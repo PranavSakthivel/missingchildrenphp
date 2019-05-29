dd00ca1fa27bff3064101f3c3570b3558575c808
Pranav Sakthivel <33013003+PranavSakthivel@users.noreply.github.com>
2019-05-28 17:04:22 -0700

    Initial sample files

---
+++ config.inc.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MKochanski
+ * Date: 7/24/2018
+ * Time: 2:47 PM
+ */
+$servername = 'vergil.u.washington.edu'; // Your Server (if noot)
+$username = 'root';
+$password = 'Team5Database';
+$database = 'missingchildren';
+$port = 17785; // Your Port
