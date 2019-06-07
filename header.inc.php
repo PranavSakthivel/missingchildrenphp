4a318cafc3122006577bc94d1a1d08031d388e7a
zachbrader <zachbrader@hotmail.com>
2019-06-06 17:30:34 -0800

    Co-authored-by: zoroasaku@gmail.com <zoroasaku@gmail.com> Co-authored-by: pranavsakthivel@outlook.com <pranavsakthivel@outlook.com>

--- header.inc.php
+++ header.inc.php
@@ -9,8 +9,8 @@
 <!-- This is a common navigation base to call different web pages on the website -->
 <div class="topnav">
     <a href="index.php">Home</a>
-    <a href="list_children.php">List Customers</a>
-    <a href="list_cases.php">List Products</a>
+    <a href="list_children.php">Missing Children List</a>
+    <a href="list_cases.php">Agencies List</a>
 </div></div>
 <h1>Sample PHP Database Program</h1>
 
