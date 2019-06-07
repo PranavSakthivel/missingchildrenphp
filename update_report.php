ef32515f04c1344966965aa4706a6feed47e0398
zachbrader <zachbrader@hotmail.com>
2019-06-06 18:30:13 -0800

    Update Update's 1st SQL

--- update_report.php
+++ update_report.php
@@ -44,12 +44,12 @@ require_once 'header.inc.php';
 
 	// Check the Request is an Update from User -- Submitted via Form
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $customerName = $_POST['customerName'];
-        if ($customerName === null)
+        $personFirstName = $_POST['personFirstName'];
+        if ($personFirstName === null)
             echo "<div><i>Specify a new name</i></div>";
-        else if ($customerName === false)
+        else if ($personFirstName === false)
             echo "<div><i>Specify a new name</i></div>";
-        else if (trim($customerName) === "")
+        else if (trim($personFirstName) === "")
             echo "<div><i>Specify a new name</i></div>";
         else {
 			
@@ -62,7 +62,7 @@ require_once 'header.inc.php';
             } else {
 				
 				// Bind user input to statement
-                $stmt->bind_param('ss', $customerName,$id);
+                $stmt->bind_param('ss', $personFirstName,$id);
 				
 				// Execute statement and commit transaction
                 $stmt->execute();
@@ -72,8 +72,14 @@ require_once 'header.inc.php';
     }
 
     /* Refresh the Data */
-    $sql = "SELECT CustomerNumber,CustomerName,StreetAddress,CityName,StateCode,PostalCode FROM Customer C " .
-        "INNER JOIN Address A ON C.defaultAddressID = A.addressID WHERE CustomerNumber = ?";
+    /*$sql = "SELECT CustomerNumber,CustomerName,StreetAddress,CityName,StateCode,PostalCode FROM Customer C " .
+        "INNER JOIN Address A ON C.defaultAddressID = A.addressID WHERE CustomerNumber = ?";*/
+    $sql = "SELECT P.personID, P.personFirstName, P.personLastName, P.personMiddleName, P.personCity, CPT.CasePersonBirthDate, CPT.CasePersonWeight, CPT.CasePersonHeight, 
+    MPC.missingPersonCaseDateMissing
+    FROM Person AS P 
+    INNER JOIN CasePersonTable AS CPT ON CPT.personID = P.personID 
+    INNER JOIN MissingPersonCase AS MPC ON CPT.missingPersonCaseID = MPC.missingPersonCaseID
+    WHERE CPT.personID = ?;";
     $stmt = $conn->stmt_init();
     if (!$stmt->prepare($sql)) {
         echo "failed to prepare";
@@ -81,17 +87,17 @@ require_once 'header.inc.php';
     else {
         $stmt->bind_param('s',$id);
         $stmt->execute();
-        $stmt->bind_result($customerNumber,$customerName,$streetName,$cityName,$stateCode,$postalCode);
+        $stmt->bind_result($personID,$firstName,$lastName,$middleName,$cityName,$birthDate,$weight,$height,$dateMissing);
         ?>
         <form method="post">
             <input type="hidden" name="id" value="<?= $id ?>">
         <?php
         while ($stmt->fetch()) {
-            echo '<a href="show_customer.php?id='  . $customerNumber . '">' . $customerName . '</a><br>' .
-             $streetName . ',' . $stateCode . '  ' . $postalCode;
+            echo '<a href="show_children.php?id='  . $personID . '">' . '</a><br>' . "Name: " . $firstName," ",$middleName," ",$lastName . PHP_EOL . 
+             $cityName . PHP_EOL . "Missing on: ",$dateMissing . PHP_EOL . "Weight: " . $weight . PHP_EOL . "height" . $height . PHP_EOL . "Birth date: " . $birthDate;
         }
     ?><br><br>
-            New Name: <input type="text" name="customerName">
+            New Name: <input type="text" name="personFirstName">
             <button type="submit">Update</button>
         </form>
     <?php
