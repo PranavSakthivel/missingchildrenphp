052c5b5003f9180d6cc52e05e0c886fddf91db3f
zachbrader <zachbrader@hotmail.com>
2019-06-11 00:59:09 -0800

    Added some comments in SearchPeople

--- search_people.php
+++ search_people.php
@@ -22,8 +22,8 @@
      
     if(strlen($query) >= $min_length){ // if query length is more or equal minimum length then
          
+        // Changes the user's query to remove any special characters. Protects against certain HTML and Javascript attacks
         $query = htmlspecialchars($query); 
-        // changes characters used in html to their equivalents, for example: < to &gt;
          
         //$query = mysqli_real_escape_string($query);
         // makes sure nobody uses SQL injection
@@ -34,48 +34,37 @@
         INNER JOIN CasePersonTable AS CPT ON CPT.missingPersonCaseID = MPC.missingPersonCaseID 
         INNER JOIN Person AS P ON CPT.personID = P.personID 
         WHERE CPT.roleCode = \"Victim\" 
-        WHERE P.personFirstName LIKE '%?%';"; 
-        $stmt = $conn->stmt_init();
-        if (!$stmt->prepare($sql)) {
+        WHERE P.personFirstName LIKE '%?%' OR P.personLastName LIKE '%?%';"; 
+
+        $stmt = $conn->stmt_init(); // This line initilizes the variable, $stmt
+        if (!$stmt->prepare($sql)) { 
             echo "failed to prepare"; // Query Breaks
         }
         else {
-            $stmt->bind_param('s',$query); // No idea
+            $stmt->bind_param('s',$query); // Replaces question marks with queries
             $stmt->execute(); // Activate SQL
             $stmt->bind_result($firstName,$lastName,$cityName,$dateMissing, $personID, $caseID);
             ?>
-            <form method="get">
+            <form method="GET">
                 <input type="hidden" name="query" value="<?= $query ?>">
             <?php
-            while ($stmt->fetch()) { // No idea
+            while ($stmt->fetch()) { // This should pull until the program hits an EOL
                 echo '<a href="show_children.php?query='  . $firstname . '">' . '</a><br>' . "Name: " . $firstName," ",$middleName," ",$lastName . "\r\n" . 
                  $cityName . "<br>" . "Missing on: ",$dateMissing . "</br>" . "Weight: " . $weight . "<br>" . "height" . $height . "</br>" . "Birth date: " . $birthDate;
             }
         }
-        // * means that it selects all fields, you can also write: `id`, `title`, `text`
-        // articles is the name of our table
-         
-        // '%$query%' is what we're looking for, % means anything, for example if $query is Hello
-        // it will match "hello", "Hello man", "gogohello", if you want exact match use `title`='$query'
-        // or if you want to match just full word so "gogohello" is out use '% $query %' ...OR ... '$query %' ... OR ... '% $query'
          
         if(mysql_num_rows($sql) > 0){ // if one or more rows are returned do following
              
             while($results = mysql_fetch_array($sql)){
-            // $results = mysql_fetch_array($sql) puts data from database into array, while it's valid it does the loop
-             
-                //echo "<p><h3>".$results['title']."</h3>".$results['text']."</p>";
-                // posts results gotten from database(title and text) you can also show id ($results['id'])
-                $stmt = $conn->stmt_init();
-                $stmt->bind_result($firstName,$lastName,$caseCity,$dateMissing,$personID,$mpcID);
+                $stmt = $conn->stmt_init(); // Initialize variable
+                $stmt->bind_result($firstName,$lastName,$caseCity,$dateMissing,$personID,$mpcID); // Allocate retrieved values
                 echo "<ul>";
                 while ($stmt->fetch()) {
                 echo '<li><a href="show_children.php?id='  . $personID . '">' . $firstName," ",$lastName,", ",$caseCity," ",$dateMissing . '</a></li>';
                 }
             }
         echo "</ul>";
-            
-             
         }
         else{ // if there is no matching rows do following
             echo "No results";
